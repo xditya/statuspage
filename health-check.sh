443e2eafedfafea2c322fca2b4fa09cf4e6ee4b6
Aditya <xditya@ultroid.tech>
2022-06-18 22:05:41 +0530

    updates - change committer - update logo - dark/light mode - some other personalisations

--- health-check.sh
+++ health-check.sh
@@ -58,9 +58,8 @@ done
 
 if [[ $commit == true ]]
 then
-  # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  git config --global user.name 'Aditya'
+  git config --global user.email 'me@xditya.me'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
